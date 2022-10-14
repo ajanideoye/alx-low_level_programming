c99470ee3da97327d7cdf342123546c3081bea28
ajanideoye <ajanideoye@gmail.com>
2022-10-14 02:27:56 -0700

    Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *Program to display on statdard output using printf function
+ *
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return(0);
+}
