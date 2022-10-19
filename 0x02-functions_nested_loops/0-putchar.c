4687393ac73b13e2cdd41fa1255bf0da7878c23b
ajanideoye <ajanideoye@gmail.com>
2022-10-19 01:59:11 -0700

    0 put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	_putchar(95);
 	_putchar(112);
-	_putchar(951);
+	_putchar(91);
 	_putchar(11);
-	_putchar(950);
+	_putchar(90);
 	_putchar(12);
-	_putchar(125);
+	_putchar(25);
 	_putchar(15);
 	_putchar('\n');
 	return (0);
