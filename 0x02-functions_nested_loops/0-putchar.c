1d12b7b8fa937a4b421592ccf92af4de2e78ded9
ajanideoye <ajanideoye@gmail.com>
2022-10-19 09:17:08 +0100

    nested loops functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Print putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns
+ * the result.
+ *
+ * @i: number one.
+ * @j: number two.
+ *
+ * Return: Add of number one and number two.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
